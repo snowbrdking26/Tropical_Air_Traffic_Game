7cc9d55d6c00d4e528917faf0b76d46fddcd0c9e
Peter <ptk726@gmail.com>
2017-11-10 22:18:57 -0500

    the dodger pieces are moving

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 $(() => {
 
-
+var rockArray = [];
 
 //Movement: moving plane1 img left right up and down;
 setInterval(movePlane, 20);
@@ -38,16 +38,18 @@ function movePlane() {
         }
     }
     stayInGameBoard();
+// move();
+// animateDiv();
+// rockMove();
+// createRock();
+
 }
 
 
-///////////////////////////////////////////////////////////////
-// Random objects to avoid
-
-
 
 ///////////////////////////////////////////////////////////////
-// Staying in the boards frame
+////----------- Staying in the boards frame----------/////////
+
 // 1. define border coordinates
 // 2. define player coordinates
 
@@ -56,59 +58,48 @@ let $plane1 = $('#plane1')[0];
 let $plane1Width = $plane1.width;
 let $plane1Height = $plane1.height;
 
+
 let $gameBoardRect = $('#board')[0].getBoundingClientRect();
     //gameboard coordiantes boundaries
-      // console.log($gameBoardRect);
-
-
 let $plane1Rect = $('#plane1')[0];
     //plane1 coordiantes boundaries
-      // console.log($plane1Rect);
-      // console.log('plane rectangle left ' + $plane1Rect.left);
-      // console.log('plane width ' + $plane1Width);
-      // console.log($gameBoardRect);
-      // console.log($plane1Rect);
 
-
-
-
-
-
+///////////////////////////////////////////////////////////////
 //////-------stay in game boundaries-----------///////
 const stayInGameBoard = () => {
       let $planeBorder = $plane1Rect.getBoundingClientRect();
       // let $gameBoardBorder = $gameBoardRect.getBoundingClientRect();
       let $planeBorderRight = $planeBorder.left + $planeBorder.width;
       let $planeBorderBottom = $planeBorder.top + $planeBorder.height;
-      let $gameBoardRectRight = $gameBoardRect.left + $gameBoardRect.width;
-      let $gameBoardRectBottom =$gameBoardRect.top + $gameBoardRect.height;
+      let $gameBoardRectRight = $gameBoardRect.left + $gameBoardRect.width-5;
+      let $gameBoardRectBottom =($gameBoardRect.top) + ($gameBoardRect.height-5);
 
           console.log($planeBorderBottom);
-          console.log($gameBoardRect.top + $gameBoardRect.height);
+          console.log($planeBorderRight);
 
 
-    if($planeBorder.left <= $gameBoardRect.left)  {
+    if($planeBorder.left < $gameBoardRect.left)  {
           console.log('inside log statement');
 
           $("#plane1").animate({left: "+=5"}, 0);
           $("#plane1").css({Transform: 'rotate(90deg)'}, 0);
     }
 
-    if($planeBorder.top <= $gameBoardRect.top)  {
+    if($planeBorder.top < $gameBoardRect.top)  {
           console.log('inside log statement');
 
           $("#plane1").animate({top: "+=5"}, 0);
           $("#plane1").css({Transform: 'rotate(180deg)'}, 0);
     }
 
-    if($planeBorderRight >= $gameBoardRectRight)  {
+    if($planeBorderRight > $gameBoardRectRight)  {
           console.log('inside log statement');
 
           $("#plane1").animate({left: "-=5"}, 0);
           $("#plane1").css({Transform: 'rotate(-90deg)'}, 0);
     }
 
-    if($planeBorderBottom >= $gameBoardRectBottom)  {
+    if($planeBorderBottom > $gameBoardRectBottom)  {
           console.log('inside log statement');
 
           $("#plane1").animate({top: "-=5"}, 0);
@@ -116,40 +107,237 @@ const stayInGameBoard = () => {
     }
 
 
-}    // end of stayInGameBoard()
+}    // end of function stayInGameBoard()
 
 
+// basic Animation of fast and slow plane bottom to top
+
+// const move = () => {
+//
+
+//
+// });
 
 
+///////////////////////////////////////////////////////////////
 //Collision with other objects, border, and collision to earn points
 
 
 
+
+
+
+// EXAMPLE 1 - Just an example I will delete this
+
+///////////////////////////////////////////////////////////////
+// Random Fast and Slow plane movements chaotic movements
+
+// $(document).ready(function(){
+//
+//
+// });
+
+// function makeNewPosition(){
+//
+//     // Get viewport dimensions (remove the dimension of the div)
+//     var h = $('#board').height();
+//     var w = $('#board').width();
+//
+//     var nh = Math.floor(Math.random() * h);
+//     var nw = Math.floor(Math.random() * w);
+//
+//     return [nh,nw];
+//
+// };
+// function makeNewPosition2(){
+//
+//     // Get viewport dimensions (remove the dimension of the div)
+//     var h = $('#board').height();
+//     var w = $('#board').width();
+//
+//     var nh2 = Math.floor(Math.random() * h);
+//     var nw2 = Math.floor(Math.random() * w);
+//
+//     return [nh2,nw2];
+//
+// };
+//
+// function animateDiv(){
+//     var newq = makeNewPosition();
+//     var newz = makeNewPosition2();
+//     var oldq = $('#planeFast').offset();
+//     var oldz = $('#planeSlow').offset();
+//     var speed = calcSpeed([oldq.top, oldq.left], newq);
+//     var speed = calcSpeed([oldq.top, oldq.left], newz);
+//
+//     $('#planeFast').animate({ top: newq[0], left: newq[1] }, speed, function(){
+//       animateDiv();
+//     });
+//     $('#planeSlow').animate({ top: newz[0], left: newq[1] }, speed, function(){
+//       animateDiv();
+//     });
+//
+// };
+//
+// function calcSpeed(prev, next) {
+//
+//     var x = Math.abs(prev[1] - next[1]);
+//     var y = Math.abs(prev[0] - next[0]);
+//
+//     var greatest = x > y ? x : y;
+//     var speedModifier = 0.1;
+//     var speed = Math.ceil(greatest/speedModifier);
+//     return speed;
+//
+// };
+
+// const move = () => {
+//
+//   const $fastPlane = $('#planeFast');
+//   $fastPlane.animate({y: -60} 2000);
+// };
+
+
+
+
+
+
+
+
+// EXAMPLE 2 - Just an example I will delete this
+
+// ////////////////////////////
+// Rock Prototype
+function createRock(id){
+  leftRight = [];
+  topBottom = [];
+  positionArray = [];
+  //have rocks spawn between these numbers
+  randRight = Math.floor(Math.random() * (600 - 580) + 580)
+  randBot = Math.floor(Math.random() * (600 - 580) + 580)
+  randTop = Math.floor(Math.random() * (30 - 20) + 20)
+  randLeft = Math.floor(Math.random() * (30 - 20) + 20)
+
+  leftPos = Math.floor(Math.random() * (400 - 15) + 15)
+  topPos = Math.floor(Math.random() * (400 - 15) + 15)
+
+  pos = Math.floor(Math.random() * (3) + 1)
+  neg = Math.floor(Math.random() * (-3) - 1)
+
+  leftRight.push(randRight, randLeft)
+  topBottom.push(randTop, randBot)
+
+  function rand01() {
+    return Math.round(Math.random())
+  }
+
+	positionArray.push(leftRight[rand01()],topBottom[rand01()])
+  clone = positionArray[rand01()]
+
+	if (clone === positionArray[0]) {
+    positionArray.splice(1, 1)
+    positionArray.push(topPos)
+  }
+
+	if (clone === positionArray[1]) {
+    positionArray.splice(0, 1)
+    positionArray.unshift(leftPos)
+  }
+
+  // Rock quadrant for directions
+  if (positionArray[0] <= (-20)) {
+    if (positionArray[1] <= 302) {
+      console.log(this);
+      this.newPos = [pos, pos];
+    }
+    if (positionArray[1] >= 303) {
+      this.newPos = [pos, neg];
+    }
+  }
+
+  if (positionArray[1] <= (-20)) {
+    if (positionArray[0] <= 502) {
+      this.newPos = [pos, pos];
+    }
+    if (positionArray[0] >= 503) {
+      this.newPos = [neg, pos];
+    }
+  }
+
+  if (positionArray[0] >= 1020) {
+    if (positionArray[1] <= 302) {
+      this.newPos = [neg, pos];
+    }
+    if (positionArray[1] >= 303) {
+      this.newPos = [neg, neg];
+    }
+  }
+
+  if (positionArray[1] >= 620) {
+    if (positionArray[0] <= 502) {
+      this.newPos = [pos, neg];
+    }
+    if (positionArray[0] >= 503) {
+      this.newPos = [neg, neg];
+    }
+  }
+
+	this.speed = 3;
+	this.width = 25;
+	this.height = 25;
+	this.left = positionArray[0];
+	this.top = positionArray[1];
+	this.id = id;
+
+	$rock = $("<div class=rock/>")
+		.css({"border":"4px solid red","height":this.height,"width":this.width,"left":this.left+"px","top":this.top+"px","position":"absolute"})
+  $('#board').append($rock);
+}
+
+
+rockInterval =
+    setInterval(function() {
+      for (var i = 0; i < 2; i++) {
+      rockArray.push(new createRock(i))
+      }
+    }, 2000)
+
+
+    // function rockMove() {
+    //   $rocks = $('.rock');
+    //
+    //   for (i = 0; i < $rocks.length; i++) {
+    //     rockTop = parseInt($rocks[i].style.top);
+    //     rockLeft = parseInt($rocks[i].style.left);
+    //
+    //     leftR = rockArray[i].newPos[0];
+    //     topR = rockArray[i].newPos[1];
+    //
+    //     newTop = rockTop + topR;
+    //     this.$rocks[i].style.top = newTop + "px"
+    //     newLeft = rockLeft + leftR;
+    //     this.$rocks[i].style.left = newLeft + "px"
+    //
+    //   }
+    //
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }); // End of the game
-
-
-// possibly useful---------------------------------------------------
-// if((plane1Rect.top+plane1Height) >= gameBoardRect.top){
-//   console.log("true2");
-//     if((plane1Rect.right+plane1Width) <= gameBoardRect.right){
-//       console.log("true3");
-//         if((plane1Rect.bottom+plane1Height) <= gameBoardRect.bottom){
-//           console.log("true4");
-//             Coords always inside parent div
-//
-//            plane1.top = 0px;
-//            plane1.left = -35px;
-//
-//            plane1.top = '' + (parseInt(plane1.top, 10) + plane1Height) + 'px';
-//                 console.log(plane1.top);
-//
-//            plane1.left = '' + (parseInt(plane1.left, 10) + plane1Width) + 'px';
-//               console.log(parseInt(plane1.left, 10));
-//
-//         }
-//     }
-// }
-// end of possibly useful---------------------------------------------------
 
 
 
@@ -157,6 +345,13 @@ const stayInGameBoard = () => {
 
 ////////////////////////////////////////////////////////////////
 //------------------------references---------------------------
+
+// random movement in box
+//https://stackoverflow.com/questions/10385950/how-to-get-a-div-to-randomly-move-around-a-page-using-jquery-or-css
+
+//for obstacle movement I used the following webpage answer move retangle back and forth
+// http://www.java2s.com/Tutorials/Javascript/Canvas_How_to/Animation/Move_rectangle_back_and_forth.htm
+
 //rotating div
 //https://stackoverflow.com/questions/382591/rotating-a-div-element-in-jquery
 
